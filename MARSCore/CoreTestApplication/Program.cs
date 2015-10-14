27519b42042ec92a31502ab45877dab3ec1e1a3a
Shanis <Shanis394042@mail.ru>
2015-10-14 23:42:32 +0300

    Add console application for testing. (Unit test not works)

---
+++ MARSCore/CoreTestApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CoreTestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var marsCore = new MARSCore.MarsCryptor();
+            uint[] key = { 0x5094d03, 0x33c4b0e, 0x396f59a, 0x254fec6 };
+            marsCore.Key = key;
+
+            uint[] data = { 0x48eb689, 0x12c312f, 0x20be8e5, 0x5268f58 };
+            var enc = marsCore.Encrypt(data);
+            var dec = marsCore.Decript(enc);
+
+            for(int i = 0; i < data.Length; i++)
+                Console.WriteLine(data[i]+"  " + dec[i]);
+
+            Console.ReadLine();
+        }
+    }
+}
